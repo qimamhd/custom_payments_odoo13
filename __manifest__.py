9516f4cfcc88e75356fee3d623e6a050ff86d367
Mohammed Ali <bamalliss84@gmail.com>
2023-02-28 18:33:53 +0300

    new update

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr_attendance', 'sale'],
+    'depends': ['base','account', 'hr_attendance', 'sale'],
 
     # always loaded
     'data': [
