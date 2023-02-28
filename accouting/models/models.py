4700f13133e88378d16d6822975cb82fc0424511
Mohammed Ali <bamalliss84@gmail.com>
2023-02-28 16:43:44 +0300

    new update

--- accouting/models/models.py
+++ accouting/models/models.py
@@ -72,7 +72,7 @@ class custom_payment(models.Model):
     total_cr = fields.Float(string="Total Credit", readonly=True, compute='_calc_local_amount')
     account_move_seq = fields.Char()
     check_auto_posted = fields.Boolean(compute="_default_auto_posted_policy")
-    sales_man_id = fields.Many2one('custom.sales.mans', string='مندوب المبيعات')
+    # sales_man_id = fields.Many2one('custom.sales.mans', string='مندوب المبيعات')
     branch_id = fields.Many2one('custom.branches', string='الفرع', copy=False, readonly="True", required=True,
                                 default=lambda self: self.env.user.branch_id.id)
 
