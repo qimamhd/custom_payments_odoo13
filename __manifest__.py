c3e0f3221bea4438581096db166060d299c6b597
Mohammed Ali <bamalliss84@gmail.com>
2023-02-28 14:59:18 +0300

    new update

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr_attendance', 'sale','qimamhd_sales_forms'],
+    'depends': ['base', 'hr_attendance', 'sale'],
 
     # always loaded
     'data': [
